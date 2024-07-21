3c4f3ca92281abfa2f6fba71c87337ab158f943b
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-07-21 15:36:04 +0530

    agency

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
-  return (
-    <div>Agency</div>
-  )
-}
+const page = async () => {
+  const authUser = await currentUser();
+  if (!authUser) {
+    return redirect("/sign-in");
+  }
+  const agencyId = await verifyAndAcceptInvitation();
 
-export default page
+  // get user details
+  const user = await getAuthUserDetails()
+  return <div>Agency</div>;
+};
+
+export default page;
